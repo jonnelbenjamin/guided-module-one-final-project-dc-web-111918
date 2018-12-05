ad956a3eab3d1b4d180ea4a326da35353e295326
github username <github email address>
2018-12-05 15:09:38 -0500

    CLI app works through first few responses. Still needs work.

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,44 +1,47 @@
 require 'pry'
 class CLI
 
-attr_accessor :runtime
+  attr_accessor :runtime
 
-def call
-  self.start
-end
+  def call
+    self.start
+  end
 
-#------Starts our application and asks for user's name and available free time
-#------Then based on free time, application searches for movie
-def start
-  puts "Hello! What is your name?"
-  user_name = gets.strip.capitalize
-  puts "Hello #{user_name}! How much free time do you have in minutes?"
+  #------Starts our application and asks for user's name and available free time
+  #------Then based on free time, application searches for movie
+  def start
+    puts "Hello! What is your name?"
+    user_name = gets.strip.capitalize
+    puts "Hello #{user_name}! How much free time do you have in minutes?"
 
-  free_time = gets.strip
-  search_for_movie(free_time)
-end
+    free_time = gets.strip
+    # search_for_movie(free_time)
+    choose_movie(free_time)
+  end
 
-def search_for_movie(free_time)
-  available_movies = []
-  free_time = free_time.to_i
-  desired_movies = Movie.all.select{|movie| movie.runtime <= free_time}
-  available_movies << desired_movies
-  puts available_movies.flatten[0].title
-end
-#search_for_movie end
+  def search_for_movie(free_time)
+    free_time = free_time.to_i
+    desired_movies = Movie.all.select{|movie| movie.runtime <= free_time}
+    return desired_movies
+  end #search_for_movie end
 
-# def choose_movie
-#   puts "How does this sound?"
-#   puts "Respond with Yes or No!"
-#   response = gets.strip
-#   if response.downcase == "yes"
-#   puts "Have fun! Don't spoil the ending!:)"
-# else available_movies.flatten.delete.first
-#   puts "How about this one?"
-#   puts available_movies.flatten[0].title
-#   binding.pry
-# end
-# end
+  def choose_movie(free_time)
+    available_movies = search_for_movie(free_time)
+    puts available_movies[0].title
+    puts "How does this sound?"
+    puts "Respond with Yes or No"
+    response = gets.strip
+    if response.downcase == "no"
+      available_movies.delete_at(0)
+      puts "How about this one?"
+      puts available_movies[0].title
+    elsif response.downcase == "yes"
+      puts "Have fun! Don't spoil the ending!:)"
+    else
+      puts "You're too picky!"
+    end
+    # binding.pry
+  end
 
 
 
